430da676f58aec5e9dad97d985000f99674c0e4d
marlon Felipe Passos <marlonpassos@agenciam3.com>
2022-06-04 15:59:50 -0300

    chore: deixando o .eslint visivel por padrão

--- src/lib/calculator.ts
+++ src/lib/calculator.ts
@@ -3,7 +3,7 @@
  */
 export function sum(a: number, b: number): number {
  if (a === Infinity || b === Infinity ) {
-   throw new Error("we can't handle ininite values");
+   throw new Error('we can\'t handle ininite values');
    
  }
  
