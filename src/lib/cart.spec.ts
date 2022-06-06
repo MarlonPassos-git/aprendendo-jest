6701eefef5d7ca6df9ade5e6bc9dcb8b8a37d374
marlon Felipe Passos <marlonpassos@agenciam3.com>
2022-06-06 16:26:21 -0300

    test(cart): should be able to add two products at the same time

--- src/lib/cart.spec.ts
+++ src/lib/cart.spec.ts
@@ -2,6 +2,21 @@ import { Cart } from './cart'
 
 describe('cart1', () => {
   let cart: Cart
+  const item1 = {
+		product: {
+			title: 'Product 1',
+			price: 1000,
+		},
+		quantity: 2,
+	}
+
+  const item2 = {
+		product: {
+			title: 'Product 2',
+			price: 12345,
+		},
+		quantity: 3,
+	}
 
   beforeEach(() => {
     cart = new Cart()
@@ -16,20 +31,22 @@ describe('cart1', () => {
   })
 
   it('should be able to add an item to cart', () => {
-		const item = {
-			product: {
-				title: 'Product 1',
-				price: 1000,
-			},
-			quantity: 2,
-		}
+		cart.add(item1)
 
-		cart.add(item)
-
-    expect(cart.getListProducts()).toEqual([item])
+    expect(cart.getListProducts()).toEqual([item1])
     expect(cart.getTotalItens()).toEqual(1)
     expect(cart.getTotalPrice()).toEqual(2000)
-	})
+  })
+  
+  it('should be able to add two products at the same time', () => {
+    cart.add(item1)
+    cart.add(item2)
+
+    expect(cart.getListProducts()).toEqual([item1, item2])
+    expect(cart.getTotalItens()).toEqual(2)
+    expect(cart.getTotalPrice()).toEqual(12345 * 3 + 1000 * 2)
+
+  })
 
 })
 // ssas 
