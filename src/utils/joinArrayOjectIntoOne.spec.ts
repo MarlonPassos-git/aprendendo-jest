dd64d02af9110bbdc624c74a17c810633633d2c6
marlon Felipe Passos <marlonpassos@agenciam3.com>
2022-06-06 00:18:59 -0300

    test(joinArrayOjectIntoOne): testando uma junção simples

---
+++ src/utils/joinArrayOjectIntoOne.spec.ts
@@ -0,0 +1,17 @@
+import { joinArrayOjectIntoOne } from './joinArrayOjectIntoOne'
+
+describe('Junção de objetos', () => {
+  it('deve juntar os 3 objeto em apenas 1 ', () => {
+    const arrayOfObjects = [
+      {
+        nome: 'marlon',
+    },
+      {
+        idade: '23',
+      },
+      {
+        color: 'blue',
+      },
+    ]
+  })
+})
