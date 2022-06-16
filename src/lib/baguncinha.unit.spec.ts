ff3f35048dd16e5dc4dece459f0e6771eb5268fb
marlon Felipe Passos <marlonpassos@agenciam3.com>
2022-06-16 18:40:29 -0300

    feat: aprendendo a usar toBeCalled

--- src/lib/baguncinha.unit.spec.ts
+++ src/lib/baguncinha.unit.spec.ts
@@ -6,5 +6,16 @@ describe('Baguncinha', () => {
   it('should conter um texto', () => {
     expect(frasezona()).toContain('Baguncinha')
   })
+  
+  it('funciona com um objeto ?', () => {
+    expect({
+      nome: 'Baguncinha',
+      idade: '13212a',
+    }).toContain({nome: 'Baguncinha'})
+  })
+
+  fit('funciona com um objeto ?', () => {
+		expect(['marlon', 'felipe', 'passos']).toContain('felipe')
+	})
 })
  
---
+++ src/lib/dirijaCarro.ts
@@ -0,0 +1,9 @@
+/**
+ * Se pessoa vai poder dirigir o carro
+ */
+export function dirijaCarro(callback: () => unknown, idade: number) {
+  const maiorIdade = idade >= 18
+  if (!maiorIdade) return
+    
+  callback()
+}
